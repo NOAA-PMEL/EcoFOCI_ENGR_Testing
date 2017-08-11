ec7812a0617aad9136ac1096ae7b9203566fb471
shaunwbell <shaun.w.bell@gmail.com>
2017-08-10 17:49:07 -0700

    don't explicitly catch connector errors

--- oer_glider/io_utils/EcoFOCI_db_io.py
+++ oer_glider/io_utils/EcoFOCI_db_io.py
@@ -68,13 +68,16 @@ class EcoFOCI_db_oculus(object):
         try:
             self.db = mysql.connector.connect(**db_config)
         except mysql.connector.Error as err:
+          """
           if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with your user name or password")
           elif err.errno == errorcode.ER_BAD_DB_ERROR:
             print("Database does not exist")
           else:
             print(err)
-        
+          """
+          print("error - will robinson")
+          
         self.db.set_converter_class(NumpyMySQLConverter)
 
         # prepare a cursor object using cursor() method
