59d6b7c0e3bb437d0b09c8c0f5c3e34adceb07a7
shaunwbell <shaun.bell@noaa.gov>
2017-08-14 09:06:50 -0700

    script update

--- oer_glider/scripts/all_dives.sh
+++ oer_glider/scripts/all_dives.sh
@@ -14,9 +14,10 @@ done
 '
 
 #Fall 2017 Deployment
-data_dir="/Users/bell/ecoraid/2017/Profilers/OculusGliders/bering_sea_fall17/sg401/p401*.nc"
-prog_dir="/Users/bell/Programs/Python/EcoFOCI_ENGR_Testing/oer_glider/"
-cal_file="/Users/bell/Programs/Python/EcoFOCI_ENGR_Testing/oer_glider/inst_config/spring_test_2017.yaml"
+root_path="/home/pavlof/bell"
+data_dir="/home/ecoraid/data/2017/Profilers/OculusGliders/bering_sea_fall17/sg401/p4011*.nc"
+prog_dir="${root_path}/Programs/Python/EcoFOCI_ENGR_Testing/oer_glider/"
+cal_file="${root_path}/Programs/Python/EcoFOCI_ENGR_Testing/oer_glider/inst_config/spring_test_2017.yaml"
 
 for data_file in $data_dir
 do
