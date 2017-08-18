ed7158a152b95b919ecd224893e8a269bdb2bbba
shaunwbell <shaun.bell@noaa.gov>
2017-08-18 08:01:43 -0700

    update scripts

--- oer_glider/scripts/all_dives.sh
+++ oer_glider/scripts/all_dives.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #Fall 2017 Deployment
-root_path="/home/pavlof/bell"
+#root_path="/home/pavlof/bell"
+root_path="/Volumes/WDC_internal/Users/bell"
 prog_dir="${root_path}/Programs/Python/EcoFOCI_ENGR_Testing/oer_glider/"
 
 state_file="EcoFOCI_config/2017_sg401_state.yaml"
